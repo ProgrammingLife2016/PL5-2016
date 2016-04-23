512d9b27618485a77f9e7345be67fbf5219a62e9
xcwfwf44 <xcwfwf44@gmail.com>
2016-04-23 21:37:07 +0200

    implemented simple RESTful JSON node querying

--- tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/Node.java
+++ tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/Node.java
@@ -1,14 +1,14 @@
 package com.pl.tagc.tagcwebapp;
 
-import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+
   public class Node {
     public String info;
     public int x;
     public int y;
-        
-    public Node() {} // JAXB needs this
+ 
+
+	public Node() {} // JAXB needs this
   
     public Node(String info, int x, int y) {
       this.info = info;
--- tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/NodeService.java
+++ tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/NodeService.java
@@ -1,28 +1,49 @@
-
 package com.pl.tagc.tagcwebapp;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
+
 // The Java class will be hosted at the URI path "/getnodes"
 @Path("/getnodes")
 public class NodeService {
 
-    
-    // The Java method will process HTTP GET requests
-    @GET 
-    // The Java method will produce content identified by the MIME Media
-    // type "application/json"
-    @Produces("application/json")
-    public Node  smooth(
-    	    @DefaultValue("0") @QueryParam("xleft") int xleft,
-    	    @DefaultValue("0") @QueryParam("ytop") int ytop,
-    	    @DefaultValue("100") @QueryParam("xright") int xright,
-    	    @DefaultValue("10") @QueryParam("ybtm") int ybtm)
-    {
-		return null;
-    	}
+	private final List<Node> cList = NodeList.getInstance();
+
+	// The Java method will process HTTP GET requests
+	@GET
+	// The Java method will produce content identified by the MIME Media
+	// type "application/json"
+	@Produces("application/json")
+	public ResultObject smooth(@DefaultValue("0") @QueryParam("xleft") int xleft,
+			@DefaultValue("0") @QueryParam("ytop") int ytop, @DefaultValue("100") @QueryParam("xright") int xright,
+			@DefaultValue("100") @QueryParam("ybtm") int ybtm) {
+		ResultObject r = getNodes(xleft, ytop, xright, ybtm);
+		return r;
+	}
+
+	private ResultObject getNodes(int xleft, int ytop, int xright, int ybtm) {
+		System.out.println(ytop);
+		System.out.println(xleft);
+		System.out.println(xright);
+		System.out.println(ybtm);
+		CopyOnWriteArrayList<Node> res= new CopyOnWriteArrayList<Node>();
+		for(Node n: cList)
+		{
+			if(n.x < xright && n.x > xleft && n.y > ytop && n.y < ybtm)
+			{
+				res.add(n);
+			}
+		}
+		
+		ResultObject reso = new ResultObject(res);
+		System.out.println(reso.getcList().size());
+		return reso;
+
+	}
 }
