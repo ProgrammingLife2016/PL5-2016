39c2b72814d73b8bab07094ac6826b6ef60a8832
jeffreyhelgers <J.M.Helgers@student.tudelft.nl>
2016-04-28 23:29:42 +0200

    Started testing.

--- backEnd/src/test/java/genomeTest/DataContainerTest.java
+++ backEnd/src/test/java/genomeTest/DataContainerTest.java
@@ -2,25 +2,25 @@ package genomeTest;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import genome.Edge;
+import genome.Genome;
+import genome.Node;
+import genome.DataContainer;
 
 /**
  * Created by Matthijs on 24-4-2016.
  */
 public class DataContainerTest {
 
+	private DataContainer data;
+	
+	/**
+	 * Setting up the DataContainer.
+	 */
     @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @Test
-    public void testAddNode() throws Exception {
-
-    }
-
-    @Test
-    public void testAddEdge() throws Exception {
-
+    public void setUp(){
+    	data = new DataContainer();
     }
 }
