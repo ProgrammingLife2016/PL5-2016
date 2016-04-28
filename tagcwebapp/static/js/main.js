9aa883b42b344962783815c61ebf501a842465e6
Thomas Oomens <thomas.oomens@live.nl>
2016-04-28 15:53:37 +0200

    Updated zoom with x and y zoom

--- tagcwebapp/static/js/main.js
+++ tagcwebapp/static/js/main.js
@@ -2,6 +2,10 @@
 var animationSpeed = 0;
 var currentHover = null;
 var zoomTimeout = null;
+var url = 'http://localhost:9998/app/';
+var ratio = 0;
+var minHeight = 300;
+var yZoom = 1;
 
 $('document').ready(function() {
 
@@ -10,39 +14,20 @@ $('document').ready(function() {
     //Update splashscreen
     splashScreen(function() {
         $('body').bind('mousewheel DOMMouseScroll', function(e) {
-            if ($(currentHover).hasClass('genome')) {
+            if ($(currentHover).hasClass('mainGenome')) {
                 e.preventDefault();
-                var slider = $('.slider', currentHover);
-                var currentWidth = slider.width();
-                var maxWidth = slider.data('maxwidth');
-                var newWidth = 0;
-                if (e.originalEvent.wheelDelta > 0) {
-                    newWidth = Math.max(Math.min(currentWidth * 0.95, currentWidth - 2), 1);
-                }
-                else {
-                    newWidth = Math.min(Math.max(currentWidth * 1.05, currentWidth + 2), maxWidth);
-                }
-                var left = parseInt(slider.css('left').replace('px', ''));
-                left = Math.max(0, left + parseInt((currentWidth - newWidth) / 2));
-                if ((left + newWidth) > maxWidth) {
-                    left = maxWidth - newWidth;
-                }
-                slider.css('left', left + 'px');
-                slider.width(parseInt(newWidth) +'px');
-                clearTimeout(zoomTimeout);
-                zoomTimeout = setTimeout(function() { updateZoom() }, 500);
+                zoom(e.originalEvent.wheelDelta);
             }
         });
 
         //Add the slider
         var genomeSlider = $('#genome').find('.slider');
         $(genomeSlider)
-            .data('maxwidth', $(genomeSlider).width())
             .draggable({
                 containment: "parent",
                 stop: function() {
                     clearTimeout(zoomTimeout);
-                    zoomTimeout = setTimeout(function() { updateZoom() }, 500);
+                    zoomTimeout = setTimeout(function() { updateZoomedGenome() }, 500);
                 }
             });
 
@@ -101,25 +86,69 @@ function translate(x, y) {
 
 }
 
-function updateZoom()
+function zoom(direction) {
+    var slider = $('.slider', currentHover);
+    var genome = $('#genome');
+    var zoomedGenome = $('#zoomedGenome');
+    var currentWidth = slider.width();
+    var currentHeight = slider.height();
+    var maxWidth = genome.width();
+    var maxHeight = genome.height();
+    var zoomRatio = zoomedGenome.height() / zoomedGenome.width();
+    var newWidth = 0;
+    if (direction > 0) {
+        newWidth = Math.max(Math.min(currentWidth * 0.95, currentWidth - 2), 1);
+    }
+    else {
+        newWidth = Math.min(Math.max(currentWidth * 1.05, currentWidth + 2), maxWidth);
+    }
+    var newHeight = Math.max(currentWidth * zoomRatio, 1);
+    if (newHeight > maxHeight) {
+        newWidth = maxHeight / zoomRatio;
+        newHeight = maxHeight;
+    }
+    var left = parseInt(slider.css('left').replace('px', ''));
+    left = Math.max(0, left + parseInt((currentWidth - newWidth) / 2));
+    if ((left + newWidth) > maxWidth) {
+        left = maxWidth - newWidth;
+    }
+    var top = parseInt(slider.css('top').replace('px', ''));
+    top = Math.max(0, top + parseInt((currentHeight - newHeight) / 2));
+    if ((top + newHeight) > maxHeight) {
+        top = maxHeight - newHeight;
+    }
+    slider.css('left', left + 'px');
+    slider.css('top', top + 'px');
+    slider.width(parseInt(newWidth) +'px');
+    slider.height(parseInt(newHeight) +'px');
+    clearTimeout(zoomTimeout);
+    //zoomTimeout = setTimeout(function() { updateZoomedGenome() }, 500);
+}
+
+function updateZoomedGenome()
 {
     var slider = $('#genome .slider');
     var x = Math.floor(slider.position().left - slider.parent().position().left);
     var y = Math.floor(slider.position().top - slider.parent().position().top);
     var width = slider.width();
     var height = slider.height();
-    drawNodes(x, y, width, height, drawZoom)
+    getNodes(x, y, width, height, drawZoom)
     console.log(x);
     console.log(y);
 }
 
 function getNodes(x, y, width, height, callback) {
 
-    $.getJSON('../getnodes', {
-        'xleft' : x,
-        'xright' : x + width,
-        'ytop' : y,
-        'ybtm' : y + height
+    $.ajax({
+        url: url + '../getnodes',
+        dataType: 'JSON',
+        type: 'GET',
+        data: {
+            'xleft': x,
+            'xright': x + width,
+            'ytop': y,
+            'ybtm': y + height
+        }
     }, function(data) {
         callback(data);
     });
@@ -131,11 +160,22 @@ function initialize() {
 
 function initializeBasicGenome() {
     $.ajax({
-        url: url + 'getDimensions',
-        dataType: 'Json',
-        type: 'POST'
+        url: url + 'getdimensions',
+        dataType: 'JSON',
+        type: 'GET'
     }).done(function(data) {
-        var slider = $('#genome .slider');
+        ratio = data.height / data.width;
+        var genome = $('#genome');
+        var height = genome.width() * ratio;
+        if (height < minHeight) {
+            yZoom = Math.floor(minHeight / height);
+            height *= yZoom;
+        }
+
+        genome.height(height);
+
+        var slider = genome.find('.slider');
+
         getNodes(0, 0, slider.width(), slider.height(), function(nodes) {
             drawGenome(nodes);
             drawZoom(nodes);
