c862060e8d7b52ce8769dfe0cd72771de727f6f4
xcwfwf44 <xcwfwf44@gmail.com>
2016-04-29 23:48:20 +0200

    fixed conflict in main.js, i divided the node weight by a smaller number to make the nodes more visible

--- tagcwebapp/static/js/main.js
+++ tagcwebapp/static/js/main.js
@@ -86,7 +86,7 @@ function draw(data, c, translate) {
         ctx.beginPath();
         var coor = translate(value.x, value.y);
         ctx.lineWidth = value.weight / 20;
-        ctx.arc(coor.x, coor.y, value.weight / 10, 0, 2 * Math.PI);
+        ctx.arc(coor.x, coor.y, value.weight / 3, 0, 2 * Math.PI);
         ctx.stroke();
         $.each(value.edges, function(key, edge) {
             if (edge.targetX != -1 && edge.targetY != -1) {
