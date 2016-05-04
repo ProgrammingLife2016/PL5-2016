c67a6c85f486c87d46b7b4a9243c9b57b15f0d95
xcwfwf44 <xcwfwf44@gmail.com>
2016-05-04 15:03:56 +0200

    added proper path annotations so the REST service can be accesed by

--- tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/NodeService.java
+++ tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/NodeService.java
@@ -10,21 +10,23 @@ import javax.ws.rs.QueryParam;
 import genome.DataContainer;
 import genome.Node;
 
-// The Java class will be hosted at the URI path "/getnodes"
-@Path("/getnodes")
+//The Java class will be hosted at the URI path "/getnodes"
+@Path("/api")
 public class NodeService {
-
+	
 	private final HashMap<Integer, Node> cList = DataContainer.DC.getNodes();
 
 	// The Java method will process HTTP GET requests
 	@GET
 	// The Java method will produce content identified by the MIME Media
 	// type "application/json"
+	@Path("/getnodes")
 	@Produces("application/json")
-	public NodeListObject requestNodes(@DefaultValue("0") @QueryParam("xleft") double xLeft,
-			@DefaultValue("0") @QueryParam("ytop") double yTop, @DefaultValue("100") @QueryParam("xright") double xRight,
-			@DefaultValue("100") @QueryParam("ybtm") double yBottom) {
-		return getNodes(xLeft, yTop, xRight, yBottom);
+	public NodeListObject requestNodes(@DefaultValue("0") @QueryParam("xleft") double xleft,
+			@DefaultValue("0") @QueryParam("ytop") double ytop, @DefaultValue("100") @QueryParam("xright") double xright,
+			@DefaultValue("100") @QueryParam("ybtm") double ybtm) {
+		NodeListObject r = getNodes(xleft, ytop, xright, ybtm);
+		return r;
 	}
 
 	// The Java method will process HTTP GET requests
