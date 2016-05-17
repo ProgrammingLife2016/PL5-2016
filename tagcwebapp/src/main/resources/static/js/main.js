d261475758211b0fc487ff22a56d66428cc93622
Thomas Oomens <thomas.oomens@live.nl>
2016-05-17 22:52:34 +0200

    Style completely renewed

--- tagcwebapp/src/main/resources/static/js/main.js
+++ tagcwebapp/src/main/resources/static/js/main.js
@@ -6,57 +6,120 @@ var url = 'http://localhost:9998/';
 var ratio = 0;
 var minHeight = 300;
 var yZoom = 1;
+var screenWidth = $(window).width();
+var screenHeight = $(window).height();
+var screenResizeTimeout, treeRedrawTimeout;
+var zoomWidth = 100;
+var cachedNodes;
+var currentMousePos = { x: -1, y: -1 };
+
+function screenResize() {
+    if ($(window).width() != screenWidth) {
+        $("#phylogenyContainer").width(Math.ceil($("#phylogenyContainer").width() * $(window).width() / screenWidth));
+        screenWidth = $(window).width();
+    }
+
+    if ($(window).height() != screenHeight) {
+        console.log(screenHeight);
+        console.log($(window).height());
+        $("#zoom").height(Math.ceil($("#zoom").height() * $(window).height() / screenHeight));
+        screenHeight = $(window).height();
+    }
+
+    //Update height
+    var borderHeight = parseInt($("#zoom").css("border-bottom-width").replace('px', ''));
+    $('#sub').height($(window).height() - $("#zoom").height() - $("#header").height() - borderHeight);
+    $('#sub').height($(window).height() - $("#zoom").height() - $("#header").height() - borderHeight);
+    if ($('#zoom').find('canvas').length) {
+        $('#zoom').find('canvas').height = $('#zoom').height();
+    }
+
+    //Update sub width
+    var borderWidth = parseInt($("#phylogenyContainer").css("border-right-width").replace('px', ''));
+    $('#minimapContainer').width($("#sub").width() - $("#phylogenyContainer").width() - borderWidth);
+    if ($('#minimapContainer').find('canvas').length) {
+        $('#minimap').find('canvas')[0].width = $('#minimap').width();
+        $('#minimap').find('canvas')[0].height = $('#minimap').height();
+        drawGenome(null); //Update the canvas
+        zoom(1, 0);
+        if (treeRedrawTimeout) {
+            clearTimeout(treeRedrawTimeout);
+        }
+        treeRedrawTimeout = setTimeout(function() {
+            drawTree(null); //Update the phylogenyTree
+        }, 1500);
+    } 
+}
 
 $('document').ready(function() {
 
-		$('#zoomoutbtn').on('click', function() {
-		 		zoom(-100);
-		 	});
-		 	$('#zoominbtn').on('click', function() {
-		 		zoom(100);
-		 	});
+    screenResize();
 
-    //Update splashscreen
-    splashScreen(function() {
-        $('body').bind('mousewheel DOMMouseScroll', function(e) {
-            if ($(currentHover).hasClass('mainGenome genome')) {
-                e.preventDefault();
-                zoom(e.originalEvent.wheelDelta);
+    $("#zoom").resizable({
+        handles: 's'
+    });
+    $("#phylogenyContainer").resizable({
+        handles: 'e'
+    });
+    $('#zoom').resize(function() {
+        screenResize();
+    });
+    $('#phylogenyContainer').resize(function() {
+        screenResize();
+    });
+
+    $(window).resize(function() {
+        if (screenResizeTimeout) {
+            clearTimeout(screenResizeTimeout);
+        }
+        screenResizeTimeout = setTimeout(function() {
+            screenResize();
+        }, 100);
+    });
+
+
+    $('#zoomoutbtn').on('click', function() {
+        zoom(-1, 10);
+    });
+    $('#zoominbtn').on('click', function() {
+        zoom(1, 10);
+    });
+
+
+    $('body').bind('mousewheel DOMMouseScroll', function(e) {
+        if ($(currentHover).is('#minimap')) {
+            e.preventDefault();
+            zoom(e.originalEvent.wheelDelta, 1);
+        }
+    });
+
+    //Add the slider
+    var slider = $('#minimap').find('.slider');
+    $(slider)
+        .draggable({
+            containment: "parent",
+            stop: function() {
+                clearTimeout(zoomTimeout);
+                zoomTimeout = setTimeout(function() { updatezoomWindow() }, 500);
             }
         });
 
-        //Add the slider
-        var slider = $('#minimap').find('.slider');
-        $(slider)
-            .draggable({
-                containment: "parent",
-                stop: function() {
-                    clearTimeout(zoomTimeout);
-                    zoomTimeout = setTimeout(function() { updatezoomWindow() }, 500);
-                }
-            });
-
-        $('.genome').hover(function() {
-            currentHover = this;
-        }, function() {
-            currentHover = null;
-        });
-
-        initialize();
+    $('.genome').hover(function() {
+        currentHover = this;
+    }, function() {
+        currentHover = null;
     });
+
+    $('#minimapContainer').mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+    });
+
+    initialize();
 });
 
-function splashScreen(callback)
-{
-    $('#splashscreen')
-        .fadeIn(animationSpeed)
-        .animate({'left':'0', 'top':'0', 'width':'300px'}, animationSpeed, function() {
-            var width = ($( window ).width() - 2) + 'px';
-            $('#header').show().animate({'left':'0', 'top':'0', 'width':width}, animationSpeed, function() {
-                $('#container').fadeIn();
-                callback();
-            })
-        });
+function resizeBlocks() {
+
 }
 
 function pxToInt(css) {
@@ -75,15 +138,23 @@ var drawZoom = function(nodes) {
 };
 
 var drawGenome = function(nodes) {
+    if (nodes == null) {
+        nodes = cachedNodes;
+    } else {
+        cachedNodes = nodes;
+    }
     draw(nodes, $('#minimap canvas')[0], function(x, y) {
         return {
-            x: x / ratio,
-            y: y / ratio
+            x: x / ratio * 50,
+            y: y / ratio * 50
         }
     });
 };
 
 function draw(data, c, translate) {
+    if (typeof c == "undefined") {
+        return;
+    }
     var points = data.cList;
     var ctx = c.getContext("2d");
     ctx.clearRect(0, 0, c.width, c.height);
@@ -106,43 +177,44 @@ function draw(data, c, translate) {
     });
 }
 
-function zoom(direction) {
+function zoom(direction, zoomAmount) {
     var slider = $('.slider', currentHover);
     var minimap = $('#minimap');
     var zoomWindow = $('#zoomWindow');
-    var currentWidth = slider.width();
-    var currentHeight = slider.height();
     var maxWidth = minimap.width();
-    var maxHeight = minimap.height();
-    var zoomRatio = zoomWindow.height() / zoomWindow.width();
-    var newWidth = 0;
-    if (direction > 0) {
-        newWidth = Math.max(Math.min(currentWidth * 0.95, currentWidth - 2), 1);
-    }
-    else {
-        newWidth = Math.min(Math.max(currentWidth * 1.05, currentWidth + 2), maxWidth);
-    }
-    var newHeight = Math.max(currentWidth * zoomRatio, 1);
-    if (newHeight > maxHeight) {
-        newWidth = maxHeight / zoomRatio;
-        newHeight = maxHeight;
-    }
+    var currentWidth = slider.width();
+    zoomWidth += (direction > 0)?-1*zoomAmount:zoomAmount;
+    zoomWidth = Math.max(1, Math.min(zoomWidth, 100));
+
+    var newWidth = Math.max(1, Math.min(maxWidth, zoomWidth / 100 * maxWidth));
+
     var left = parseInt(slider.css('left').replace('px', ''));
-    left = Math.max(0, left + parseInt((currentWidth - newWidth) / 2));
-    if ((left + newWidth) > maxWidth) {
-        left = maxWidth - newWidth;
+    var difference = Math.abs(currentWidth - newWidth);
+
+    if (direction > 0) {
+        var mouseX = currentMousePos.x - minimap.position().left;
+        var leftSide = mouseX - left;
+        var rightSide = left + currentWidth - mouseX;
+        var dif = Math.min(difference, leftSide - rightSide);
+        if (dif > 0) {
+            left += dif + (difference - dif) / 2;
+        } else {
+            left += Math.max(0, difference + dif) / 2;
+        }
+    } else {
+        left -= difference / 2;
     }
-    var top = parseInt(slider.css('top').replace('px', ''));
-    top = Math.max(0, top + parseInt((currentHeight - newHeight) / 2));
-    if ((top + newHeight) > maxHeight) {
-        top = maxHeight - newHeight;
-    }
+
+    left += Math.min(0, maxWidth - (left + newWidth)); //Fix slider going out of right screen.
+    left = Math.max(0, Math.min(maxWidth, left));
     slider.css('left', left + 'px');
-    slider.css('top', top + 'px');
     slider.width(parseInt(newWidth) +'px');
-    slider.height(parseInt(newHeight) +'px');
     clearTimeout(zoomTimeout);
-    zoomTimeout = setTimeout(function() { updatezoomWindow() }, 500);
+    if (zoomAmount != 0) {
+        zoomTimeout = setTimeout(function () {
+            updatezoomWindow();
+        }, 500);
+    }
 }
 
 function updatezoomWindow()
@@ -161,12 +233,12 @@ function updatezoomWindow()
 // an object with left right top and bottom properties.  
 function computeBoundingBox(x, y, width, height)
 {
-	return {
-            'xleft': x * ratio,
-            'xright': (x + width) * ratio,
-            'ytop': y / yZoom * ratio,
-            'ybtm': (y + height) / yZoom * ratio
-        }
+    return {
+        'xleft': x * ratio,
+        'xright': (x + width) * ratio,
+        'ytop': y / yZoom * ratio,
+        'ybtm': (y + height) / yZoom * ratio
+    }
 }
 
 function getNodes(boundingBox, callback) {
@@ -204,12 +276,12 @@ function initializeMinimap() {
 
         ratio = data.width / minimap.width();
 
-        minimap.prepend(
+        minimap.find('.canvasContainer').html(
             $('<canvas/>', {'class':'genomeCanvas', Width: minimap.width(), Height: minimap.height() })
         );
         var boundingBox = computeBoundingBox(0, 0, minimap.width(), minimap.height());
         getNodes(boundingBox, drawGenome);
-        zoom(-1);
+        zoom(-1, 1);
     });
 }
 
@@ -218,4 +290,13 @@ function initializeZoomWindow() {
     zoomWindow.prepend(
         $('<canvas/>', {'class':'zoomedCanvas', Width: zoomWindow.width(), Height: zoomWindow.height() })
     );
+}
+
+function fullSizeMinimap() {
+    $('#phylogenyContainer').animate({
+        'width': 30
+    }, 1000, function() {
+        zoom(1, 0);
+        screenResize();
+    });
 }
