7fbe48dc95ccd1eb3766b2be725ecff869c9512a
nogeenaccount <user@wlan-145-94-209-218.wlan.tudelft.nl>
2016-05-17 11:28:26 +0200

    All data is now loaded into the database, with the exception of the phylogenetic tree.

--- backEnd/src/main/java/database/Database.java
+++ backEnd/src/main/java/database/Database.java
@@ -1,26 +1,24 @@
 package database;
 
 import controller.Controller;
-import genome.Genome;
-import genome.Strand;
 import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import java.io.File;
 import java.sql.Connection;
+import java.util.Objects;
 
 /**
- * @author Jeffrey Helgers.
+ * @author user.
  *     This class creates and uses the database.
  */
 public class Database {
 
-    private Connection connection;
-    private String name;
+    //private Connection connection;
     private String path;
-    private Controller controller;
+    //private Controller controller;
     private GraphDatabaseService graphDb;
 
     private static String username = "postgres";
@@ -32,14 +30,16 @@ public class Database {
      * @param data Input for the database.
      */
     public Database(String databasePath, Controller data) {
-        path = databasePath;
-        deleteDatabase();
+        path = databasePath.toLowerCase();
 
-        name = databasePath.toLowerCase();
-        controller = data;
+        //controller = data;
         createDatabaseConnection(databasePath);
 
-        insertPhylogeneticTree();
+        //Populate the database with constraints, indexes and actual data
+        createIndexes();
+        createConstraints();
+        insertNodes();
+        insertEdges();
     }
 
     /**
@@ -48,7 +48,9 @@ public class Database {
      */
     private void createDatabaseConnection(String databasePath) {
         System.out.println("setting up connection");
-        graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(new File(databasePath));
+        GraphDatabaseBuilder dbBuilder = new GraphDatabaseFactory().
+                newEmbeddedDatabaseBuilder(new File(databasePath));
+        graphDb = dbBuilder.newGraphDatabase();
         System.out.println("Opened database successfully");
     }
 
@@ -56,6 +58,8 @@ public class Database {
      * Delete the database.
      */
     public void deleteDatabase() {
+        //shut down the embedded database server
+        graphDb.shutdown();
         deleteDirectory(new File(path));
     }
 
@@ -65,34 +69,80 @@ public class Database {
      * @param   dir  the filepath to delete
      */
     private void deleteDirectory(File dir) {
+        assert dir != null;
+
         if (dir.exists()) {
             File[] files = dir.listFiles();
+            assert files != null;
+
             for (int i = 0; i < files.length; i++) {
                 if (files[i].isDirectory()) {
+                    //call recursively if it's a directory
                     deleteDirectory(files[i]);
                 }
                 else {
-                    files[i].delete();
+                    //delete file otherwise
+                    boolean deleted = files[i].delete();
                 }
             }
+        }
+
+        //delete the current directory
+        if (!dir.delete()) {
+            System.out.println("Something went wrong deleting the directory " + dir);
         }
     }
 
     /**
-     * Inserts the phylogenetic tree into the database.
+     * Creates the constraints for the database we're using.
      */
-    public void insertPhylogeneticTree() {
-        Node n;
-
+    private void createConstraints() {
         try (Transaction tx = graphDb.beginTx()) {
-            for (Genome genome : controller.getGenomes().values()) {
-                for (Strand strand : genome.getStrands()) {
-                    n = graphDb.createNode();
-                    n.setProperty("sequence", strand.getSequence());
-                    n.setProperty("name", strand.getId());
-                }
-            }
+
             tx.success();
         }
+    }
+
+    /**
+     * Creates the indexes for the database we're using.
+     */
+    private void createIndexes() {
+        try (Transaction tx = graphDb.beginTx()) {
+            graphDb.execute("CREATE INDEX ON :Strand(id)");
+            tx.success();
+        }
+    }
+
+    /**
+     * Inserts the nodes into the database.
+     */
+    private void insertNodes() {
+        try (Transaction tx = graphDb.beginTx()) {
+            graphDb.execute("LOAD CSV WITH HEADERS FROM \'" + new File("data/nodes.csv").toURI()
+                    + "\' AS csvLine\nCREATE (p:Strand { id: toInt(csvLine.id), sequence: "
+                    + "csvLine.sequence })");
+            tx.success();
+        }
+    }
+
+    /**
+     * Inserts the edges into the database.
+     */
+    private void insertEdges() {
+        try (Transaction tx = graphDb.beginTx()) {
+            graphDb.execute("LOAD CSV WITH HEADERS FROM \'" + new File("data/edges.csv").toURI()
+                    + "\' AS csvLine\nMATCH (start:Strand { id: toInt(csvLine.start)}),"
+                    + "(end:Strand { id: toInt(csvLine.end)})\n"
+                    + "CREATE (start)-[:GENOME { genome: csvLine.genome }]->(end)");
+            tx.success();
+        }
+    }
+
+    /**
+     * GraphDatabaseService getter.
+     * @return GraphDatabaseService.
+     */
+    public GraphDatabaseService getGraphService() {
+        return graphDb;
     }
 }
--- backEnd/src/test/java/database/DatabaseTest.java
+++ backEnd/src/test/java/database/DatabaseTest.java
@@ -5,9 +5,15 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphdb.Transaction;
+import org.neo4j.tooling.GlobalGraphOperations;
 import parser.Parser;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by user on 11/05/16.
@@ -24,15 +30,105 @@ public class DatabaseTest {
     public void setup() {
         Controller dc = Parser.parse("data/test2.gfa");
         db = new Database("test.db", dc);
+        dc.createNodesCSV("data/nodes.csv");
+        dc.createEdgesCSV("data/edges.csv");
     }
 
     /**
      * The actual tests, currently checks if file indeed gets created.
      */
     @Test
-    public void test() {
+    public void testDBCreation() {
         File f = new File("test.db");
         Assert.assertTrue(f.exists());
+    }
+
+    /**
+     * Test if all nodes from the test file get inserted.
+     */
+    @Test
+    public void testNodeInsertion() {
+        Iterable<Node> allNodes;
+        String[] strands = {"", "AAAAAAAA", "A", "C", "T", "G", "GG", "TA", "GG",
+                "CGATGCAA", "CG", "TAG", "AT", "GAG", "CAG", "ATA"};
+
+        int i = 1;
+        try (Transaction tx = db.getGraphService().beginTx()) {
+            //Fetch all nodes from the database
+            allNodes = GlobalGraphOperations.at(db.getGraphService()).getAllNodes();
+            tx.success();
+
+            //Check if all nodes are inserted correctly in the database
+            for (Node a : allNodes) {
+                Assert.assertEquals(a.getProperty("id"), (long) i);
+                Assert.assertEquals(a.getProperty("sequence"), strands[i]);
+                i++;
+            }
+        }
+
+        //Make sure no extra nodes are inserted
+        Assert.assertEquals(16, i);
+    }
+
+    /**
+     * Test if all nodes from the test file get inserted.
+     */
+    @Test
+    public void testEdgeInsertion() {
+        Iterable<Relationship> allRelas;
+        Set<String> strands = new HashSet<>();
+        strands.add("1|2 AA");
+        strands.add("2|3 AA");
+        strands.add("3|10 AA");
+        strands.add("10|11 AA");
+        strands.add("11|12 AA");
+        strands.add("12|13 AA");
+        strands.add("13|14 AA");
+        strands.add("14|15 AA");
+        strands.add("1|3 BB");
+        strands.add("3|5 BB");
+        strands.add("5|7 BB");
+        strands.add("7|8 BB");
+        strands.add("8|9 BB");
+        strands.add("9|12 BB");
+        strands.add("12|13 BB");
+        strands.add("13|15 BB");
+        strands.add("1|3 CC");
+        strands.add("3|5 CC");
+        strands.add("5|6 CC");
+        strands.add("6|8 CC");
+        strands.add("8|9 CC");
+        strands.add("9|12 CC");
+        strands.add("12|15 CC");
+        strands.add("1|3 DD");
+        strands.add("3|5 DD");
+        strands.add("5|6 DD");
+        strands.add("6|9 DD");
+        strands.add("9|12 DD");
+        strands.add("12|15 DD");
+        strands.add("1|3 EE");
+        strands.add("3|4 EE");
+        strands.add("4|12 EE");
+        strands.add("12|15 EE");
+
+        int i = 1;
+
+        try (Transaction tx = db.getGraphService().beginTx()) {
+            //fetch all relationships from the graph
+            allRelas = GlobalGraphOperations.at(db.getGraphService()).getAllRelationships();
+
+            //check if all relationships/edges are indeed in the database
+            for (Relationship a : allRelas) {
+                String coordinates = a.getStartNode().getProperty("id") + "|"
+                        + a.getEndNode().getProperty("id");
+                Assert.assertTrue(strands.contains(coordinates + " " + a.getProperty("genome")));
+                i++;
+            }
+            tx.success();
+        }
+
+        //Make sure no extra edges are inserted
+        Assert.assertEquals(i, 34);
     }
 
     /**
@@ -41,5 +137,7 @@ public class DatabaseTest {
     @After
     public void deleteDB() {
         db.deleteDatabase();
+        File f = new File("test.db");
+        Assert.assertTrue(!f.exists());
     }
 }
