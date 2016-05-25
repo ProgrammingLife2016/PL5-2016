b41dda1531f7a38ad50849ebcba5e33b13c147ef
nogeenaccount <user@wlan-145-94-208-51.wlan.tudelft.nl>
2016-05-25 16:00:02 +0200

    Created a caching mechanism

---
+++ backEnd/src/main/java/Cache.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+/**
+ * Created by user on 25/05/16.
+ */
+public class Cache {
+    float zoomLevel;
+    int leftBound;
+    int rightBound;
+
+    public Cache(){
+
+    }
+
+    public void addGenomes(List<String> genomes) {
+
+    }
+
+    public void removeGenomes(List<String> genomes) {
+
+    }
+}
