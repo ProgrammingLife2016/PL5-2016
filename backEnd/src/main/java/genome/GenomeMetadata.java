f97622223a853919731634d2f117d9e0bde48eda
xcwfwf44 <xcwfwf44@gmail.com>
2016-06-02 13:12:32 +0200

    added javadoc to GenomeMetadata class

--- backEnd/src/main/java/genome/GenomeMetadata.java
+++ backEnd/src/main/java/genome/GenomeMetadata.java
@@ -1,20 +1,41 @@
 package genome;
 
+/**
+ * The Class GenomeMetadata.
+ */
 public class GenomeMetadata {
 
+	/** The lineage. */
 	private String lineage;
+	
+	/** The genome id. */
 	private String genomeId;
 	
+	/**
+	 * Instantiates a new genome metadata.
+	 *
+	 * @param genomeId the genome id
+	 * @param lineage the lineage
+	 */
 	public GenomeMetadata(String genomeId, String lineage) {
 		this.genomeId = genomeId;
 		this.lineage = lineage;
 	}
 
+	/**
+	 * Gets the lineage.
+	 *
+	 * @return the lineage
+	 */
 	public String getLineage() {
-		// TODO Auto-generated method stub
 		return lineage;
 	}
 
+	/**
+	 * Gets the genome id.
+	 *
+	 * @return the genome id
+	 */
 	public String getGenomeId() {
 		return genomeId;
 	}
