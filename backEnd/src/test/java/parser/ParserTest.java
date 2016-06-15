fdf2d00aa2f3e0dace0eb2e3e4dbe402bb78ab9c
jeffreyhelgers <j.m.helgers@student.tudelft.nl>
2016-06-15 13:06:27 +0200

    Test for parsing the annotations.

--- backEnd/src/test/java/parser/ParserTest.java
+++ backEnd/src/test/java/parser/ParserTest.java
@@ -1,11 +1,14 @@
 package parser;
 
 import genome.GenomeGraph;
+import genome.GenomicFeature;
+
 import org.junit.Test;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
@@ -98,5 +101,16 @@ public class ParserTest {
 			e.printStackTrace();
 			fail();
 		}
+	}
+	
+	/**
+	 * Test the annotationParser.
+	 */
+	@Test
+	public void testParseAnnotations() {
+		GenomicFeature feature = Parser.parseAnnotations("data/annotationsTest.gff").get(0); 
+		assertEquals(feature.getStart(), 2627279);
+		assertEquals(feature.getEnd(), 2632941);
+		assertEquals(feature.getDisplayName(), "displayName=DS9 5662 bp (4 orfs 2 IGs)  (electronically transferred)");
 	}
 }
