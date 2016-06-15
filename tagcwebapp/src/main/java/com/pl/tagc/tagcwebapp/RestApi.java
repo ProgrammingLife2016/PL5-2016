1f5a9fcde8c7f11f949d6a96b1c4441574a59c7e
Thomas Oomens <thomas.oomens@live.nl>
2016-06-15 09:49:06 +0200

    Build in searchField finding Mutations + fixed coords

--- tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/RestApi.java
+++ tagcwebapp/src/main/java/com/pl/tagc/tagcwebapp/RestApi.java
@@ -55,8 +55,12 @@ public class RestApi {
      */
     @POST
     @Path("/setactivegenomes")
-    public ArrayListObject setActiveGenomes(@FormParam("names[]") List<String> ids) {
-        return BackEndAdapter.getInstance().setActiveGenomes(ids);
+    public Response setActiveGenomes(@FormParam("names[]") List<String> ids) {
+        return Response.ok() //200
+                .entity(BackEndAdapter.getInstance().setActiveGenomes(ids))
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
     }
 
     /**
@@ -83,9 +87,12 @@ public class RestApi {
     @GET
     @Path("/search")
     @Produces("application/json")
-    public SearchResultObject search(@QueryParam("searchString") String searchString,
+    public Response search(@QueryParam("searchString") String searchString,
                                      @QueryParam("searchType") String searchType) {
-        return BackEndAdapter.getInstance().search(searchString, searchType);
-
+        return Response.ok() //200
+                .entity(BackEndAdapter.getInstance().search(searchString, searchType))
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+                .allow("OPTIONS").build();
     }
 }
